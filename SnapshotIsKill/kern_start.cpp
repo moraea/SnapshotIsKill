739f9a684ca052290b4927221f00c6f85f5c37f8
flagersgit <48706788+flagersgit@users.noreply.github.com>
2022-02-07 05:01:19 -0500

    Implement DoNotSeal (UNTESTED)

--- SnapshotIsKill/kern_start.cpp
+++ SnapshotIsKill/kern_start.cpp
@@ -7,17 +7,20 @@
 
 #include <Headers/plugin_start.hpp>
 #include <Headers/kern_patcher.hpp>
+#include <Headers/kern_file.hpp>
 #include <Headers/kern_api.hpp>
+
+#include <libkern/c++/OSUnserialize.h>
 
 // Boot args.
 static const char *bootargOff[] {
-    "-snapshotiskilloff"
+    "-sskilloff"
 };
 static const char *bootargDebug[] {
-    "-snapshotiskilldbg"
+    "-sskilldbg"
 };
 static const char *bootargBeta[] {
-    "-snapshotiskillbeta"
+    "-sskillbeta"
 };
 
 #define SNAPSHOT_OP_CREATE 0x01
@@ -38,11 +41,47 @@ int wrapFsSnapshot(SInt64 a1, SInt64 uap_op) {
     }
 };
 
+static const char *plistHeader {
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+    "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n"
+    "<plist version=\"1.0\">\n"
+};
+
+static const char *plistFooter {
+    "\n</plist>\n"
+};
+
+#define kUpdateOptionsPlist "/macOS Install Data/UpdateOptions.plist"
+
+mach_vm_address_t orgVfsSwitchRoot {};
+int wrapVfsSwitchRoot(const char *incoming_vol_old_path, const char *outgoing_vol_new_path, uint32_t flags) {
+    uint8_t *buffer {nullptr};
+    size_t bufferSize {0};
+    if (NULL != (buffer = FileIO::readFileToBuffer(kUpdateOptionsPlist, bufferSize))) {
+        OSObject *parsedXml = NULL;
+        if (NULL != (parsedXml = OSUnserializeXML((const char *)buffer, bufferSize))) {
+            OSDictionary *updateOptionsDict = NULL;
+            updateOptionsDict = OSDynamicCast(OSDictionary, parsedXml);
+            if (updateOptionsDict->setObject("DoNotSeal", kOSBooleanTrue)) {
+                OSSerialize *serializer = OSSerialize::withCapacity(0x20000);
+                serializer->addString(plistHeader);
+                updateOptionsDict->serialize(serializer);
+                serializer->addString(plistFooter);
+                FileIO::writeBufferToFile(kUpdateOptionsPlist, serializer->text(), serializer->getLength());
+            }
+        }
+    }
+    
+    if (buffer) { Buffer::deleter(buffer); }
+    
+    return FunctionCast(wrapVfsSwitchRoot, orgVfsSwitchRoot)(incoming_vol_old_path, outgoing_vol_new_path, flags);
+};
+
 // Plugin configuration.
 PluginConfiguration ADDPR(config) {
     xStringify(PRODUCT_NAME),
     parseModuleVersion(xStringify(MODULE_VERSION)),
-    LiluAPI::AllowNormal,
+    LiluAPI::AllowNormal | LiluAPI::AllowInstallerRecovery | LiluAPI::AllowSafeMode,
     bootargOff,
     arrsize(bootargOff),
     bootargDebug,
@@ -52,9 +91,10 @@ PluginConfiguration ADDPR(config) {
     KernelVersion::BigSur,
     KernelVersion::Monterey,
     []() {
-        LiluAPI::Error error = lilu.onPatcherLoad([](void *user, KernelPatcher &patcher) {
+        lilu.onPatcherLoad([](void *user, KernelPatcher &patcher) {
             KernelPatcher::RouteRequest requests[] {
-                KernelPatcher::RouteRequest("_fs_snapshot", wrapFsSnapshot, orgFsSnapshot)
+                KernelPatcher::RouteRequest("_fs_snapshot", wrapFsSnapshot, orgFsSnapshot),
+                KernelPatcher::RouteRequest("_vfs_switch_root", wrapVfsSwitchRoot, orgVfsSwitchRoot)
             };
             
             if (!patcher.routeMultipleLong(KernelPatcher::KernelID, requests, 1)) { PANIC("sskill", "failed to route"); };
